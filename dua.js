3282aacab097a4904fe59318787b2e9446e8cbc9
Muhammad Iqbal <iqbalshedow25@gmail.com>
2021-08-06 20:25:23 +0700

    Add files via upload
    pertemuan_1

---
+++ dua.js
@@ -0,0 +1,37 @@
+let month = 1 
+if (month == 1){
+  console.log(month, " = January");
+}
+else if(month == 2) {
+  console.log(month, "= February");
+}
+else if(month == 3) {
+  console.log(month, "= March");
+}
+else if(month == 4) {
+  console.log(month, "= April");
+}
+else if(month == 5) {
+  console.log(month, "= Mei");
+}
+else if(month == 6) {
+  console.log(month, "= June");
+}
+else if(month == 7) {
+  console.log(month, "= July");
+}
+else if(month == 8) {
+  console.log(month, "= August");
+}
+else if(month == 9) {
+  console.log(month, "= September");
+}
+else if(month == 10) {
+  console.log(month, "= October");
+}
+else if(month == 11) {
+  console.log(month, "= November");
+}
+else if(month == 12) {
+  console.log(month, "= December");
+}
---
+++ tiga.js
@@ -0,0 +1,5 @@
+let i = 0;
+while (i<20){
+  console.log(i+2, " - Saya akan menjadi Backend Engineer");
+  i+=2;
+}
